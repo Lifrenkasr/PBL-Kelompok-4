569ce8370485ccd3051659e27fcf0ad30b683872
unknown <muhammadabdurrochman210305@gmail.com>
2023-12-29 04:43:42 +0700

    tambah crud jadwal

--- project/app.py
+++ project/app.py
@@ -23,6 +23,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
+class Jadwal:
+    def __init__(self, id, day, time, class_name):
+        self.id = id
+        self.day = day
+        self.time = time
+        self.class_name = class_name
+
 # Rute untuk halamam login
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -97,32 +104,106 @@ def dashboard():
 
         profile_picture = user_data[0] if user_data and len(user_data) > 0 else 'default_profile.png'
         
-        if 'username' in session:
-                # Koneksi ke database
-            cur = mysql.connection.cursor()
-            cur.execute("SELECT * FROM access_log ORDER BY log_time DESC LIMIT 100")  # Ambil 100 log terakhir
-            access_logs = cur.fetchall()
-            cur.close()
+    if 'username' in session:
+            # Koneksi ke database
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM access_log ORDER BY log_time DESC LIMIT 1000")  # Ambil 10 log terakhir
+        access_logs = cur.fetchall()
+        cur.close()
 
         return render_template('dashboard.html', access_logs=access_logs, profile_picture=profile_picture)
     else:
         return redirect(url_for('login'))
     
-@app.route('/jadwal')
-def jadwal():
-    if 'username' in session:
-        # Koneksi ke database
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT profile_picture FROM users WHERE username = %s", (session['username'],))
-        user_data = cur.fetchone()
-        cur.close()
+# Rute untuk menampilkan daftar jadwal
+@app.route('/jadwal', methods=['GET'])
+def jadwal_list():
+            if 'username' in session:
+                    # Koneksi ke database
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT profile_picture FROM users WHERE username = %s", (session['username'],))
+                user_data = cur.fetchone()
+                cur.close()
 
-        profile_picture = user_data[0] if user_data and len(user_data) > 0 else 'default_profile.png'
+                profile_picture = user_data[0] if user_data and len(user_data) > 0 else 'default_profile.png'
+
+                
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT id, day, time, class FROM jadwal")
+                jadwal_list = [Jadwal(id=row[0], day=row[1], time=row[2], class_name=row[3]) for row in cur.fetchall()]
+                cur.close()
+                
+                return render_template('jadwal_list.html', jadwal_list=jadwal_list, profile_picture=profile_picture)
+            else: 
+                return redirect(url_for('login'))
 
 
-        return render_template('jadwal.html', profile_picture=profile_picture)
+@app.route('/jadwal/add', methods=['GET', 'POST'])
+def add_jadwal():
+            if 'username' in session:
+                    # Koneksi ke database
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT profile_picture FROM users WHERE username = %s", (session['username'],))
+                user_data = cur.fetchone()
+                cur.close()
+
+                profile_picture = user_data[0] if user_data and len(user_data) > 0 else 'default_profile.png'
+
+               
+                if request.method == 'POST':
+                    day = request.form['day']
+                    time = request.form['time']
+                    class_name = request.form['class']
+
+                    cur = mysql.connection.cursor()
+                    cur.execute("INSERT INTO jadwal (day, time, class) VALUES (%s, %s, %s)", (day, time, class_name))
+                    mysql.connection.commit()
+                    cur.close()
+                    flash('Jadwal berhasil ditambahkan.')
+                    return redirect(url_for('jadwal_list'))
+
+                return render_template('add_jadwal.html',add_jadwal=add_jadwal, profile_picture=profile_picture)
+            else: 
+                return redirect(url_for('login'))
+
+
+@app.route('/jadwal/edit/<int:jadwal_id>', methods=['GET', 'POST'])
+def edit_jadwal(jadwal_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM jadwal WHERE id = %s", (jadwal_id,))
+    jadwal_data = cur.fetchone()
+    cur.close()
+
+    if jadwal_data:
+        jadwal = Jadwal(id=jadwal_data[0], day=jadwal_data[1], time=jadwal_data[2], class_name=jadwal_data[3])
+
+        if request.method == 'POST':
+            day = request.form['day']
+            time = request.form['time']
+            class_name = request.form['class']
+
+            cur = mysql.connection.cursor()
+            cur.execute("UPDATE jadwal SET day = %s, time = %s, class = %s WHERE id = %s",
+                        (day, time, class_name, jadwal_id))
+            mysql.connection.commit()
+            cur.close()
+            flash('Jadwal berhasil diubah.')
+            return redirect(url_for('jadwal_list'))
+
+        return render_template('edit_jadwal.html', jadwal=jadwal)
+
     else:
-        return redirect(url_for('login'))
+        flash('Jadwal tidak ditemukan.')
+        return redirect(url_for('jadwal_list'))
+
+@app.route('/jadwal/delete/<int:jadwal_id>')
+def delete_jadwal(jadwal_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM jadwal WHERE id = %s", (jadwal_id,))
+    mysql.connection.commit()
+    cur.close()
+    flash('Jadwal berhasil dihapus.')
+    return redirect(url_for('jadwal_list'))
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
